0a816dd9fbcdd37a40edc58b1d99390f0ed1ee5a
Anton <work111acc@gmail.com>
2020-10-30 14:44:52 +0200

    TI-dac5571 driver first release

---
+++ dac5571.c
@@ -0,0 +1,174 @@
+/**
+ * @copyright Private code.
+ *
+ * @file    dac5571.c
+ * @author  Anton K.
+ * @date    30 Oct 2020
+ *
+ * @details Driver for TI-DAC5571 Source code
+ */
+
+#include <stddef.h>
+#include <string.h>
+
+#include "dac5571.h"
+
+#define DAC5571_RESOLUTION_MASK     0xFF
+
+#define DAC5571_REG_BYTE_COUNT              2
+#define DAC5571_REG_DATA_HALF_BYTE_OFFSET   4
+#define DAC5571_REG_MODE_OFFSET             4
+
+struct {
+    dac5571_dev_t dev;
+    unsigned char is_init:1;
+} dac5571_handle;
+
+/**@brief Driver device structure verification
+ *
+ * @param[IN] dev - device control structure, @see dac5571_dev_t
+ *
+ * @return result code, @see DAC5571_RET_CODE_T
+ */
+static DAC5571_RET_CODE_T __dac5571_verify_device(const dac5571_dev_t * const dev)
+{
+    //! Verify I2C communication functions
+    if (dev->i2c_read == NULL || dev->i2c_write == NULL)
+        return DAC5571_RET_CODE_ERROR_INVALID_PARAM;
+
+    //! Verify I2C address
+    if (dev->i2c_addr != DAC5571_ADDR_BROADCAST && dev->i2c_addr != DAC5571_ADDR_SINGLECAST)
+        return DAC5571_RET_CODE_ERROR_INVALID_PARAM;
+
+    //! Verify reference voltage
+    if (dev->ref_voltage != DAC5571_REF_VOLT_3_3V && dev->ref_voltage != DAC5571_REF_VOLT_5V)
+        return DAC5571_RET_CODE_ERROR_INVALID_PARAM;
+
+    return DAC5571_RET_CODE_SUCCESS;
+}
+
+DAC5571_RET_CODE_T dac5571_init(const dac5571_dev_t * const dev)
+{
+    DAC5571_RET_CODE_T ret_code;
+
+    //! Check does driver already initialized
+    if (dac5571_handle.is_init) {
+        ret_code = DAC5571_RET_CODE_ERROR_INVALID_STATE;
+        goto __ret;
+    }
+
+    //! Verify parameters
+    ret_code = __dac5571_verify_device(dev);
+    if (ret_code != DAC5571_RET_CODE_SUCCESS)
+        goto __ret;
+
+    //! Copy device data to internal structure
+    memcpy(&dac5571_handle.dev, dev, sizeof(dac5571_dev_t));
+
+    dac5571_handle.is_init = 1;
+
+__ret:
+    return ret_code;
+}
+
+DAC5571_RET_CODE_T dac5571_deinit(void)
+{
+    //! Check does driver has been initialized
+    if (dac5571_handle.is_init) {
+        dac5571_handle.is_init = 0;
+        return DAC5571_RET_CODE_SUCCESS;
+    } else {
+        return DAC5571_RET_CODE_ERROR_INVALID_STATE;
+    }
+}
+
+
+DAC5571_RET_CODE_T dac5571_force_mode(const dac5571_dev_t * const dev, DAC5571_MODE_T mode)
+{
+    DAC5571_RET_CODE_T ret_code;
+    dac5571_dev_t *dev_ptr;
+    unsigned char data[2];
+
+    //! Verify mode
+    if (mode >= DAC5571_MODE_COUNT) {
+        ret_code = DAC5571_RET_CODE_ERROR_INVALID_PARAM;
+        goto __ret;
+    }
+
+    //! Verify the way of device structure usage
+    if (dac5571_handle.is_init) {
+        //! Use internal device structure
+        dev_ptr = &dac5571_handle.dev;
+    } else if (dev != NULL) {
+        //! Verify parameters
+        ret_code = __dac5571_verify_device(dev);
+        if (ret_code != DAC5571_RET_CODE_SUCCESS)
+            goto __ret;
+
+        //! Use input device structure
+        dev_ptr = (dac5571_dev_t *)dev;
+    } else {
+        ret_code = DAC5571_RET_CODE_ERROR_INVALID_STATE;
+        goto __ret;
+    }
+
+    data[0] = mode << DAC5571_REG_MODE_OFFSET;
+    data[1] = 0;
+
+    ret_code = dev_ptr->i2c_write(dev_ptr->i2c_addr, data, DAC5571_REG_BYTE_COUNT);
+    if (ret_code) {
+        ret_code = DAC5571_RET_CODE_ERROR_COMMUNICATION;
+        goto __ret;
+    }
+
+__ret:
+    return ret_code;
+}
+
+DAC5571_RET_CODE_T dac5571_force_data(const dac5571_dev_t * const dev, unsigned short mV)
+{
+    DAC5571_RET_CODE_T ret_code;
+    dac5571_dev_t *dev_ptr;
+    unsigned char data[2];
+
+    //! Verify the way of device structure usage
+    if (dac5571_handle.is_init) {
+        //! Use internal device structure
+        dev_ptr = &dac5571_handle.dev;
+    } else if (dev != NULL) {
+        //! Verify parameters
+        ret_code = __dac5571_verify_device(dev);
+        if (ret_code != DAC5571_RET_CODE_SUCCESS)
+            goto __ret;
+
+        //! Use input device structure
+        dev_ptr = (dac5571_dev_t *)dev;
+    } else {
+        ret_code = DAC5571_RET_CODE_ERROR_INVALID_STATE;
+        goto __ret;
+    }
+
+    //! Verify input voltage value
+    if (mV > dev_ptr->ref_voltage) {
+        ret_code = DAC5571_RET_CODE_ERROR_INVALID_PARAM;
+        goto __ret;
+    }
+
+    //! Calculate data bits
+    data[0] = mV * DAC5571_RESOLUTION_MASK / dev_ptr->ref_voltage;
+
+    //! Data control and voltage composition
+    data[1] = (data[0] & 0x0F) << DAC5571_REG_DATA_HALF_BYTE_OFFSET;
+    data[0] = (data[0] >> DAC5571_REG_DATA_HALF_BYTE_OFFSET) | (DAC5571_MODE_NORMAL << DAC5571_REG_MODE_OFFSET);
+
+    ret_code = dev_ptr->i2c_write(dev_ptr->i2c_addr, data, DAC5571_REG_BYTE_COUNT);
+    if (ret_code) {
+        ret_code = DAC5571_RET_CODE_ERROR_COMMUNICATION;
+        goto __ret;
+    }
+
+__ret:
+    return ret_code;
+}
+
+
---
+++ dac5571.h
@@ -0,0 +1,107 @@
+/**
+ * @copyright Private code.
+ *
+ * @file    dac5571.h
+ * @author  Anton K.
+ * @date    30 Oct 2020
+ *
+ * @details Driver for TI-DAC5571 Header file
+ */
+
+#ifndef DAC5571_DAC5571_H_
+#define DAC5571_DAC5571_H_
+
+/**@brief Driver return code type
+ */
+typedef enum {
+    DAC5571_RET_CODE_SUCCESS,
+    DAC5571_RET_CODE_ERROR_INVALID_STATE,
+    DAC5571_RET_CODE_ERROR_INVALID_PARAM,
+    DAC5571_RET_CODE_ERROR_COMMUNICATION
+} DAC5571_RET_CODE_T;
+
+/**@brief Address type. DAC5571 allowed to broadcast control and data bytes
+ *  via I2C interface to synchronous several chips on the single line.
+ */
+typedef enum {
+    DAC5571_ADDR_BROADCAST = 0xA8,
+    DAC5571_ADDR_SINGLECAST = 0xAA
+} DAC5571_ADDR_T;
+
+/**@brief Reference voltage
+ */
+typedef enum {
+    DAC5571_REF_VOLT_3_3V = 3300,
+    DAC5571_REF_VOLT_5V = 5000
+} DAC5571_REF_VOLT_T;
+
+/**@brief Control modes
+ */
+typedef enum {
+    DAC5571_MODE_NORMAL,
+    DAC5571_MODE_PWD_1K,
+    DAC5571_MODE_PWD_100K,
+    DAC5571_MODE_PWD_HZ,
+
+    DAC5571_MODE_COUNT
+} DAC5571_MODE_T;
+
+/**@brief I2C communication function pointer type
+ *
+ * @param[IN] addr - I2C address
+ * @param[IN] data - data buffer pointer
+ * @param[IN] len - data buffer length
+ *
+ * @return result code of communication, success should be as 0 code
+ */
+typedef int (* dac5571_i2c_com_fptr_t)(unsigned char addr, unsigned char *data, unsigned short len);
+
+/**@brief Device driver configuration structure type
+ */
+typedef struct {
+    const dac5571_i2c_com_fptr_t i2c_read;
+    const dac5571_i2c_com_fptr_t i2c_write;
+    const DAC5571_ADDR_T i2c_addr;
+    const DAC5571_REF_VOLT_T ref_voltage;
+} dac5571_dev_t;
+
+/**@brief Device initialization
+ * @note After using this function, driver allowed to use other functions
+ *      with NULL pointer instead of (dac5571_dev_t *)
+ *
+ * @param[IN] dev - device control structure, @see dac5571_dev_t
+ *
+ * @return
+ */
+DAC5571_RET_CODE_T dac5571_init(const dac5571_dev_t * const dev);
+
+/**@brief Device deinitialization function
+ *
+ * @return DAC5571_RET_CODE_SUCCESS - if driver was initialized before,
+ *         DAC5571_RET_CODE_ERROR_INVALID_STATE - otherwise
+ */
+DAC5571_RET_CODE_T dac5571_deinit(void);
+
+/**@brief Forcing device mode
+ *
+ * @param[IN] dev - device control structure, @see dac5571_dev_t.
+ *                  NULL pointer acceptable if dac5571_init() has been executed before.
+ * @param[IN] mode - device mode, @see DAC5571_MODE_T
+ *
+ * @return result code, @see DAC5571_RET_CODE_T
+ */
+DAC5571_RET_CODE_T dac5571_force_mode(const dac5571_dev_t * const dev, DAC5571_MODE_T mode);
+
+/**@brief Forcing to output voltage data
+ *  @note This function automatically forced DAC5571_MODE_NORMAL
+ *
+ * @param[IN] dev - device control structure, @see dac5571_dev_t.
+ *                  NULL pointer acceptable if dac5571_init() has been executed before.
+ * @param[IN] mV - output mVoltage data
+ *
+ * @return result code, @see DAC5571_RET_CODE_T
+ */
+DAC5571_RET_CODE_T dac5571_force_data(const dac5571_dev_t * const dev, unsigned short mV);
+
+
+#endif /* DAC5571_DAC5571_H_ */
